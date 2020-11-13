ec2ff882c16c825804b3960f234b0c195f05d58f
omakhlas <omarmakhlas@gmail.com>
2020-11-13 15:12:40 +0100

    [add] project structure

---
+++ src/main/java/com/wesquad/kata/foobarqix/FooBarQixApplication.java
@@ -0,0 +1,13 @@
+package com.wesquad.kata.foobarqix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooBarQixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FooBarQixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wesquad/kata/foobarqix/FooBarQixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesquad.kata.foobarqix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooBarQixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
